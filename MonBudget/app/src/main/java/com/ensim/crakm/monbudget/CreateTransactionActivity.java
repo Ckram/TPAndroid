feada4b4e24c0d7938b16a30f32761b03df3cd61
ckram <cram.leroux@gmail.com>
2016-05-07 08:59:03 +0200

    projet android

---
+++ MonBudget/app/src/main/java/com/ensim/crakm/monbudget/CreateTransactionActivity.java
@@ -0,0 +1,13 @@
+package com.ensim.crakm.monbudget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateTransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_transaction);
+    }
+}
